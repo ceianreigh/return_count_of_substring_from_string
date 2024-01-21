6dc3f294bdaf0626630011e9d4f406f6ba7c21bb
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-21 22:09:38 +0800

    Write the given string and the substring

--- return_count_of_substring_from_string.py
+++ return_count_of_substring_from_string.py
@@ -3,9 +3,13 @@
 # pseudocode
 
 # write the given string
+str_x = "Emma is good developer. Emma is a writer"
 
 # write the substring
+substr_x = "Emma"
 
+print(str_x)
+print(substr_x)
 # count the number of times the substring appears in the given string
 
 # print the number of times the substring appears in the given string

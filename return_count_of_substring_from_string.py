4b4a63d9f90227fe7975ca1e9a0244f1815f6b62
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-21 22:04:46 +0800

    Add instruction and pseudocode in the program

---
+++ return_count_of_substring_from_string.py
@@ -0,0 +1,11 @@
+# Write a program to find how many times substring “Emma” appears in the given string.
+
+# pseudocode
+
+# write the given string
+
+# write the substring
+
+# count the number of times the substring appears in the given string
+
+# print the number of times the substring appears in the given string
